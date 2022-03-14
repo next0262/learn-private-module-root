a4b358b62e5ad6b69e7f6bb3060bfbc905dc545b
wjkim <wj89.kim@samsung.com>
2022-03-14 10:56:12 +0900

    finish 2

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
-  #type        = string
-  #default     = "us-west-2"
+  type        = string
+  default     = "us-west-2"
 }
 
 variable "prefix" {
   description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
-  #type        = string
-  #default     = "pre"
+  type        = string
+  default     = "pre"
 }
 
 variable "name" {
   description = "Your name to attach to the webapp address"
-  #type        = string
-  #default     = "wjkim"
+  type        = string
+  default     = "wjkim"
 }
